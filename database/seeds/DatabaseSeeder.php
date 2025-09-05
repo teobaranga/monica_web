2f23a28937417ae20fb8a24f2215c0441d970867
Teo Baranga <139957+teobaranga@users.noreply.github.com>
2025-09-05 18:28:50 +0100

    chore(db): combine seeders into DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,26 +1,26 @@
 <?php
 
+use Database\Seeders\CurrenciesSeeder;
+use Database\Seeders\TermsSeeder;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\App;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
+    public function run(): void
     {
+        $this->call([
+            TermsSeeder::class,
+            CurrenciesSeeder::class,
+        ]);
+
         switch (App::environment()) {
+            case 'testing':
             case 'local':
                 $this->call(FakeUserTableSeeder::class);
-            break;
-            case 'testing':
-                $this->call(FakeUserTableSeeder::class);
-            break;
+                break;
             case 'production':
-            break;
+                break;
         }
     }
 }
