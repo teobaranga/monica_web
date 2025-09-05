2786102e259d80ae6031d8997cb0c2ec82d4ff6b
Teo Baranga <139957+teobaranga@users.noreply.github.com>
2025-09-05 10:59:49 +0100

    attempt to fix browser tests

--- tests/Browser/Feature/UploadVCardTest.php
+++ tests/Browser/Feature/UploadVCardTest.php
@@ -11,8 +11,6 @@ class UploadVCardTest extends DuskTestCase
     /**
      * Make sure that the Add contact view has the link to the upload screen,
      * and that the screen contains the blank view.
-     *
-     * @return void
      */
     public function test_upload_vcard_is_accessible_from_add_contact_view()
     {
@@ -29,8 +27,6 @@ class UploadVCardTest extends DuskTestCase
     /**
      * Make sure that the Import button leads to the Import screen, and that
      * the cancel button leads to the Blank import screen.
-     *
-     * @return void
      */
     public function test_import_button_leads_to_import_screen()
     {
@@ -46,8 +42,6 @@ class UploadVCardTest extends DuskTestCase
 
     /**
      * Upload a single contact from a valid vcard file.
-     *
-     * @return void
      */
     public function test_user_can_import_contacts_from_a_vcf_card()
     {
@@ -57,8 +51,12 @@ class UploadVCardTest extends DuskTestCase
 
             $browser
                 ->visit('/settings/import')
-                ->clickLink('Import vCard')
-                ->attach('vcard', base_path('tests/stubs/single_vcard_stub.vcard'))
+                ->clickLink('Import vCard');
+
+            $browser
+                ->attach('vcard', base_path('tests/stubs/single_vcard_stub.vcard'));
+
+            $browser
                 ->on(new ImportVCardUpload)
                 ->scrollTo('upload')
                 ->press('Upload');
@@ -71,19 +69,25 @@ class UploadVCardTest extends DuskTestCase
 
     /**
      * Upload a contact from a broken vCard and see that it triggers an error.
-     *
-     * @return void
      */
     public function test_user_see_error_when_importing_broken_vcard()
     {
-        $this->browse(function ($browser) {
-            $browser->login()
-                ->visit('/settings/import')
-                ->clickLink('Import vCard')
-                ->attach('vcard', base_path('tests/stubs/broken_vcard_stub.vcard'))
+        $this->browse(function (Browser $browser) {
+            $browser->login();
+
+            $browser->visit('/settings/import')
+                ->clickLink('Import vCard');
+
+            $browser
+                ->attach('vcard', base_path('tests/stubs/broken_vcard_stub.vcard'));
+
+            $browser
                 ->on(new ImportVCardUpload)
                 ->scrollTo('upload')
-                ->press('Upload')
+                ->press('Upload');
+
+            $browser
+                ->waitForText('The vcard must be a file of type: vcf, vcard.', 3)
                 ->assertSee('The vcard must be a file of type: vcf, vcard.');
         });
     }
